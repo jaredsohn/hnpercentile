480e1b176b83c898d41c1538dfe16366e7811be6
Jared Sohn <jared.sohn@gmail.com>
2014-03-03 12:41:18 -0800

    fix migration

--- db/migrate/20140303112000_algolia_changes.rb
+++ db/migrate/20140303112000_algolia_changes.rb
@@ -1,6 +1,9 @@
 class AlgoliaChanges < ActiveRecord::Migration
   def up
-  	add_column :comment_karma, :story_karma, :poll_karma, :integer
+  	add_column :member, comment_karma, :integer
+  	add_column :member, story_karma, :integer
+  	add_column :member, poll_karma, :integer
+
   end
   def down
 
