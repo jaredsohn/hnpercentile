26b92b0d895f768d84110684dcbde3736ddcc9f0
Jared Sohn <jared.sohn@gmail.com>
2014-03-04 16:46:45 -0800

    convert timestamp to string first

--- app/models/member.rb
+++ app/models/member.rb
@@ -37,7 +37,7 @@ class Member < ActiveRecord::Base
     doc = open(url).read
     j = JSON.parse(doc)
     
-    date_registered = DateTime.strptime(j['created_at_i'], "%s")
+    date_registered = DateTime.strptime(j['created_at_i'].to_s, "%s")
 
     karma = j['karma']
     date_range = (Date.today - date_registered).to_f
